19d9540e4383fda3b3fd2dba4994d04eb422d4b2
KshitijV97 <writetokshitij@gmail.com>
2022-04-10 22:06:27 +0530

    Added styling

--- src/components/Queries/Queries.js
+++ src/components/Queries/Queries.js
@@ -4,20 +4,48 @@ import { ENTRANCES } from '../../constants/entrances';
 const Queries = ({ parkingLot, setParkingLot }) => {
 	return (
 		<div>
-			<button
-				onClick={() => {
-					addCar(parkingLot, setParkingLot, ENTRANCES.FRONT);
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					padding: '0.5em',
+					margin: '0.5em',
 				}}
 			>
-				Add new car from front
-			</button>
-			<button
-				onClick={() => {
-					addCar(parkingLot, setParkingLot, ENTRANCES.REAR);
-				}}
-			>
-				Add new car from rear
-			</button>
+				<button
+					style={{
+						border: 'none',
+						textAlign: 'center',
+						textDecoration: 'none',
+						display: 'inline-block',
+						backgroundColor: '#D6CDEA',
+						padding: '15px 32px',
+						cursor: 'pointer',
+					}}
+					onClick={() => {
+						addCar(parkingLot, setParkingLot, ENTRANCES.FRONT);
+					}}
+				>
+					Add new car from front
+				</button>
+				<button
+					style={{
+						border: 'none',
+						textAlign: 'center',
+						textDecoration: 'none',
+						display: 'inline-block',
+						backgroundColor: '#D6CDEA',
+						padding: '15px 32px',
+						marginLeft: '1em',
+						cursor: 'pointer',
+					}}
+					onClick={() => {
+						addCar(parkingLot, setParkingLot, ENTRANCES.REAR);
+					}}
+				>
+					Add new car from rear
+				</button>
+			</div>
 		</div>
 	);
 };
