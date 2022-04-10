53298a8fe5d61a99b271ad34ccab55064a1079d6
KshitijV97 <writetokshitij@gmail.com>
2022-04-10 14:13:42 +0530

    Added some colors

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+	RED: '#FF0000',
+	YELLOW: '#FFFF00',
+	LIME: '#00FF00',
+	BLUE: '#0000FF',
+	MAROON: '#800000',
+	OLIVE: '#808000',
+	GREEN: '#008000',
+	PURPLE: '#800080',
+};
