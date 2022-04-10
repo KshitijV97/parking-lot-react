9fb577a4c6c4d7cb223e79a96b0543ca535f65f6
KshitijV97 <writetokshitij@gmail.com>
2022-04-10 19:02:18 +0530

    Function to add a car to the lot

---
+++ src/functions/addCar.js
@@ -0,0 +1,20 @@
+import { findSlot } from './findSlot';
+import { generateCar } from './generateCar';
+
+export const addCar = (parkingLot, setParkingLot, entrance) => {
+	let emptySlot = findSlot(parkingLot, entrance);
+	console.log('LOGS | Empty slot is ', emptySlot);
+	if (emptySlot !== -1) {
+		let car = generateCar();
+		car.ticketNumber = emptySlot;
+		console.log('Trying to add this car', car);
+		setParkingLot([
+			...parkingLot.slice(0, emptySlot),
+			car,
+			...parkingLot.slice(emptySlot + 1, parkingLot.length),
+		]);
+	} else {
+		alert('Cannot add more cars as the Parking lot is full');
+		return false;
+	}
+};
