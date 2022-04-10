81e7e440bbdb9e706cb629c7005b6a8afcbb8026
KshitijV97 <writetokshitij@gmail.com>
2022-04-10 18:24:00 +0530

    Taking Parking lot size from user, Allowing user to add car, Showing parking lot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+/**
+ * * Registration numbers of all cars of a particular Color.
+ * Ticket number in which a car with a given registration number is placed.
+ * Ticket numbers of all ticket where a car of a particular color is placed.
+ */
+
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
+import ParkingSize from './components/ParkingLotSize/ParkingLotSize';
+import ParkingLot from './components/ParkingLot/ParkingLot';
+import Queries from './components/Queries/Queries';
+
+import { initializeParking } from './functions/initializeParking';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [parkingLotSize, setParkingLotSize] = useState(0);
+
+	const [parkingLot, setParkingLot] = useState([]);
+
+	useEffect(() => {
+		if (parkingLotSize > 0) {
+			setParkingLot(initializeParking(parkingLotSize));
+		}
+	}, [parkingLotSize]);
+
+	/*
+	Logging for Debugging
+	*/
+	useEffect(() => {
+		console.log('LOGS | New parking lot size is', parkingLotSize);
+	}, [parkingLotSize]);
+
+	useEffect(() => {
+		console.log('LOGS | New parking lot is', parkingLot);
+	}, [parkingLot]);
+
+	return (
+		<div className='App'>
+			<ParkingSize setParkingLotSize={setParkingLotSize} />
+			<Queries parkingLot={parkingLot} setParkingLot={setParkingLot} />
+			<ParkingLot parkingLot={parkingLot} />
+		</div>
+	);
 }
 
 export default App;
+
+/**
+ * Car
+ * -> ticketNumber
+ * -> color
+ * -> registrationNumber
+ */
