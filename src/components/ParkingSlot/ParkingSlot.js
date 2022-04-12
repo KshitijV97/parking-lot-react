7674bc7e65809ae4acaf55a054b49f7ad32febf9
KshitijV97 <writetokshitij@gmail.com>
2022-04-12 20:03:16 +0530

    Showing color for car and added car remove functionality

--- src/components/ParkingSlot/ParkingSlot.js
+++ src/components/ParkingSlot/ParkingSlot.js
@@ -1,53 +1,110 @@
 import React, { useMemo } from 'react';
+import { COLORS } from '../../constants/colors';
 
-const ParkingSlot = ({ ticketNumber, registrationNumber, color }) => {
+const ParkingSlot = ({
+	ticketNumber,
+	registrationNumber,
+	color,
+	parkingLot,
+	setParkingLot,
+	removeCar,
+	entrance,
+}) => {
 	let occupancyStatus = useMemo(
 		() => (registrationNumber ? 'Occupied' : 'Empty'),
 		[registrationNumber]
 	);
 	return (
-		<div
-			style={{
-				margin: '1em',
-				padding: '1em',
-				display: 'flex',
-				flexDirection: 'row',
-				justifyContent: 'space-between',
-				maxWidth: '80vw',
-				backgroundColor: 'white',
-				border: 'black 1px solid',
-			}}
-		>
+		<div>
 			<div
 				style={{
+					margin: '1em',
+					padding: '1em',
 					display: 'flex',
 					flexDirection: 'column',
-					minWidth: '50%',
+					justifyContent: 'flex-start',
+					maxWidth: '80vw',
+					backgroundColor: 'white',
+					border: 'black 1px solid',
 				}}
 			>
-				<div style={{ padding: '0.5em' }}>
-					<span>Ticket number : </span>
-					<span>{ticketNumber}</span>
-				</div>
-				<div style={{ padding: '0.5em' }}>
-					<span>Occupancy status : </span>
-					<span>{occupancyStatus}</span>
-				</div>
-			</div>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					minWidth: '50%',
-				}}
-			>
-				<div style={{ padding: '0.5em' }}>
-					<span>Registration number : </span>
-					<span>{registrationNumber}</span>
-				</div>
-				<div style={{ padding: '0.5em' }}>
-					<span>Color : </span>
-					<span>{color}</span>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							minWidth: '40%',
+						}}
+					>
+						<div style={{ padding: '0.5em' }}>
+							<span>Ticket number : </span>
+							<span>{ticketNumber}</span>
+						</div>
+						<div style={{ padding: '0.5em' }}>
+							<span>Occupancy status : </span>
+							<span>{occupancyStatus}</span>
+						</div>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							minWidth: '40%',
+						}}
+					>
+						<div style={{ padding: '0.5em' }}>
+							<span>Registration number : </span>
+							<span>{registrationNumber}</span>
+						</div>
+						<div style={{ padding: '0.5em' }}>
+							<span>Color : </span>
+							<span
+								style={{
+									marginRight: '1em',
+									color: 'white',
+									backgroundColor: COLORS[color],
+									width: '1em',
+									height: '1em',
+								}}
+							>
+								_____
+							</span>
+							<span>{color}</span>
+						</div>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'row-reverse',
+							width: '100%',
+							marginRight: '-1em',
+						}}
+					>
+						<button
+							disabled={!registrationNumber}
+							style={{
+								marginRight: '1em',
+								height: '2em',
+								backgroundColor: 'white',
+							}}
+							onClick={() => {
+								removeCar(parkingLot, setParkingLot, {
+									ticketNumber: ticketNumber,
+									registrationNumber: registrationNumber,
+									color: color,
+									entrance: entrance,
+								});
+							}}
+						>
+							Remove car
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
