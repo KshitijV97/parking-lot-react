c4e2587723dd77ec062278bea1cd521216308b59
KshitijV97 <writetokshitij@gmail.com>
2022-04-12 20:02:42 +0530

    Passing required props and functions

--- src/components/ParkingLot/ParkingLot.js
+++ src/components/ParkingLot/ParkingLot.js
@@ -1,16 +1,24 @@
 import ParkingSlot from '../ParkingSlot/ParkingSlot';
+import { removeCar } from '../../functions/removeCar';
 
-const ParkingLot = ({ parkingLot }) => {
+const ParkingLot = ({ parkingLot, setParkingLot }) => {
 	return (
 		<div>
-			{parkingLot.map((slot, index) => (
-				<ParkingSlot
-					ticketNumber={slot.ticketNumber}
-					registrationNumber={slot.registrationNumber}
-					color={slot.color}
-					key={index}
-				/>
-			))}
+			{parkingLot.map((slot, index) => {
+				console.log('Logging slot', slot);
+				return (
+					<ParkingSlot
+						ticketNumber={slot.ticketNumber}
+						registrationNumber={slot.registrationNumber}
+						color={slot.color}
+						key={index}
+						parkingLot={parkingLot}
+						setParkingLot={setParkingLot}
+						removeCar={removeCar}
+						entrance={slot.entrance}
+					/>
+				);
+			})}
 		</div>
 	);
 };
